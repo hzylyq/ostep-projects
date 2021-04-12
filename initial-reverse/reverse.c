ff86ef0cc2efe41d7ab3bb927e1e6f29e58995b4
huzhiyang <164177565@qq.com>
2021-04-12 15:15:55 +0800

    fix: 反转

--- initial-reverse/reverse.c
+++ initial-reverse/reverse.c
@@ -3,10 +3,12 @@
 #include <sys/stat.h>
 #include <string.h>
 
-void handle_error(char *msg) {
-    fprintf(stderr, "%s", msg);
-    exit(EXIT_FAILURE);
-}
+#define handle_error(msg)   \
+do {                        \
+    fprintf(stderr, msg);   \
+    exit(EXIT_FAILURE);     \
+} while(0)                  \
+
 
 typedef struct link_list {
     char *line;
@@ -18,101 +20,71 @@ int main(int argc, char *argv[]) {
     in = stdin;
     out = stdout;
 
-    if (argc == 1) {
-        fprintf(stderr, "usage: reverse <input> <output>");
-        exit(EXIT_FAILURE);
-    }
-
     // reverse a.txt
     if ((argc == 2) && (fopen(argv[1], "r") == NULL)) {
-        char *msg;
-        sprintf(msg, "reverse: cannot open file '%s\n'", argv[1]);
-        handle_error(msg);
+        fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
+        exit(EXIT_FAILURE);
     }
 
     // reverse a.txt b.txt
     if (argc == 3) {
-        in = fopen(argv[1], "r");
         // 打开失败
-        if (in == NULL) {
+        if ((in = fopen(argv[1], "r")) == NULL) {
             fprintf(stderr, "reverse: can't open file '%s'\n", argv[1]);
             exit(EXIT_FAILURE);
         }
-        out = fopen(argv[2], "w");
-        if (out == NULL) {
+
+        if ((out = fopen(argv[2], "w")) == NULL) {
             fprintf(stderr, "reverse: can't open file '%s'\n", argv[2]);
             exit(EXIT_FAILURE);
         }
 
-        struct stat *s1 = malloc(sizeof(struct stat)), *s2 = malloc(sizeof(struct stat));
-        if (s1 == NULL || s2 == NULL) {
-            fprintf(stderr, "malloc failed\n");
-            exit(EXIT_FAILURE);
-        }
+        struct stat sb1, sb2;
 
-        if (fstat(fileno(in), s1) != 0) {
-            free(s1);
-            free(s2);
-            fprintf(stderr, "reverse: fstat error\n");
-            exit(EXIT_FAILURE);
-        }
-        if (fstat(fileno(out), s2) != 0) {
-            free(s1);
-            free(s2);
-            fprintf(stderr, "reverse: fstat error\n");
-            exit(EXIT_FAILURE);
+        if ((fstat(fileno(in), &sb1) != 0) || (fstat(fileno(out), &sb2) != 0)) {
+            handle_error("reverse: fstat error\n");
         }
 
         // 指向同一个文件
-        if (s1->st_ino == s2->st_ino) {
-            free(s1);
-            free(s2);
-            fprintf(stderr, "reverse: input and output file must differ\n");
-            exit(EXIT_FAILURE);
+        if (sb1.st_ino == sb2.st_ino) {
+            handle_error("reverse: input and output file must differ\n");
         }
-        free(s1);
-        free(s2);
     }
 
     if (argc > 3) {
-        fprintf(stderr, "usage: reverse <input> <output>");
-        exit(EXIT_FAILURE);
+        handle_error("usage: reverse <input> <output>");
     }
 
     // 用一个链表来存放数据
-    link_list *head = malloc(sizeof(link_list)), *curr = NULL, *prev = NULL;
-
-    if (head == NULL) {
-        fprintf(stderr, "reverse: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    head->line = NULL;
-    head->Next = NULL;
-
-    curr = head;
+    link_list *head = NULL;
     char *line = NULL;
     size_t linecap = 0;
 
-    while (getline(&line, &linecap, in) > 0) {
-        if (head->line == NULL) {
-            if ((head->line = malloc(linecap)) == NULL) {
-                free(head);
-                fprintf(stderr, "reverse: malloc failed\n");
-                exit(EXIT_FAILURE);
-            }
-            strcpy(head->line, line);
-        } else {
-            link_list *tmp = malloc(sizeof(link_list));
-            if (tmp == NULL) {
-                free(head);
-                fprintf(stderr, "reverse: malloc failed\n");
-                exit(EXIT_FAILURE);
-            }
+    //
+    while (getline(&line, &linecap, in) != -1) {
+        link_list *node = malloc(sizeof(link_list));
+        if (node == NULL) {
+            handle_error("reverse: malloc failed\n");
         }
+        if ((node->line = strdup(line)) == NULL) {
+            handle_error("reverse: strdup failed\n");
+        }
+
+        node->Next = head;
+        head = node;
     }
 
-    // todo 反转链表
+    // print reversed lines
+    while (head != NULL) {
+        link_list *tmp = head;
+        fprintf(out, "%s", head->line);
+        head = head->Next;
+        free(tmp->line);
+        free(tmp);
+    }
+
+    fclose(in);
+    fclose(out);
 
     return 0;
 }
